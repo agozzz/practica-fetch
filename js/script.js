68cced88dd48b38747c8401b0ecaca2eebdb141a
agozzz <agozorzoli@gmail.com>
2024-08-24 01:10:45 -0300

    Desplegando el JSON

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // En este archivo no utilizamos el evento "DOMContentLoaded", ya que se colocó el atributo "defer" en la importación del script,
 // que nos soluciona el problema de los elementos no cargados del DOM. Más info => https://www.w3schools.com/tags/att_script_defer.asp
 
-const DATA_URL = "json/data.json"; // URL que contiene los datos que queremos mostrar
+const DATA_URL = "json/data.json"; //URL que contiene los datos que queremos mostrar
 
 const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
 
@@ -19,3 +19,16 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL).then(response => {
+  if (response.ok) { //Verifica si la respuesta es exitosa
+    return response.json(); //Convierte a la respuesta en un objeto JavaScript
+  } else {
+    console.log("Error en la respuesta"); //Aviso del error si la respuesta no es exitosa
+  }
+})
+  .then(respuestaJSON => { //Objeto JavaScript con los datos parseados del JSON
+    showData(respuestaJSON.students); //Llama a la función showData. Obtiene el array de estudiantes dentro del objeto.
+})
+  .catch(error => {
+    console.error("Error en el fetch", error); //Manejo de error en las solicitudes
+});
